a6b69fb82e0f20fc5305ae45e449994c6fd91a4a
jospin6 <jospinndagano1@gmail.com>
2024-04-25 21:37:05 +0200

    Refactor the bubbleSort function code

--- arrays/bubbleSort.js
+++ arrays/bubbleSort.js
@@ -3,13 +3,11 @@ function bubblSort(array) {
     for (let i = 0; i < array.length; i++) {
         for (let j = i + 1; j < array.length; j++) {
             if (array[i] > array[j]) {
-                let temp = array[j]
-                array[j] = array[i]
-                array[i] = temp
+                [array[i], array[j]] = [array[j], array[i]]
             }
         }
     }
     return array
 }
-const arr = [9,4,-3,7,1,3,2]
+const arr = [9,4,-3,7,1]
 console.log(bubblSort(arr))
