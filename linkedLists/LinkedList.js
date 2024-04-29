49ba6afad6ee506cde30488f5e1df3ac5e660a96
jospin6 <jospinndagano1@gmail.com>
2024-04-29 13:21:47 +0200

    Add the addFirst and print methods to the linkedList class

--- linkedLists/LinkedList.js
+++ linkedLists/LinkedList.js
@@ -1,5 +1,28 @@
+const Node = require("./node")
+
 class LinkedList {
     constructor(){
         this.head = null
     }
+
+    addFirst(data){
+        const node = new Node(data)
+        node.next = this.head
+        this.head = node
+    }
+
+    print(){
+        let current = this.head
+        while (current) {
+            console.log(current.data)
+            current = current.next
+        }
+    }
 }
+
+const linkedList = new LinkedList()
+
+linkedList.addFirst(5)
+linkedList.addFirst(12)
+linkedList.addFirst(7)
+linkedList.print()
