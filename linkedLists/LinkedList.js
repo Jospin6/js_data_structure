0599349b6863b095925ce22cf69c925f2f518865
jospin6 <jospinndagano1@gmail.com>
2024-04-29 14:18:34 +0200

    Add addAtLast and size methods to the linkedList class

--- linkedLists/LinkedList.js
+++ linkedLists/LinkedList.js
@@ -11,6 +11,29 @@ class LinkedList {
         this.head = node
     }
 
+    addAtLast(data){
+        const node = new Node(data)
+        if (!this.head) {
+            this.head = node
+            return
+        }
+        let current = this.head
+        while (current.next) {
+            current = current.next
+        }
+        current.next = node
+    }
+
+    size(){
+        let count = 0
+        let current = this.head
+        while (current) {
+            count++
+            current = current.next
+        }
+        return count
+    }
+
     print(){
         let current = this.head
         while (current) {
@@ -25,4 +48,10 @@ const linkedList = new LinkedList()
 linkedList.addFirst(5)
 linkedList.addFirst(12)
 linkedList.addFirst(7)
+linkedList.addAtLast(10)
+linkedList.addFirst(2)
 linkedList.print()
+
+console.log("==============")
+
+console.log("List Size: " + linkedList.size())
