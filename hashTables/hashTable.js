805005ba7542c2c462a84d3b9b7b361fa44ded97
jospin6 <jospinndagano1@gmail.com>
2024-05-20 12:10:18 +0200

    Implement hashTable

--- hashTables/hashTable.js
+++ hashTables/hashTable.js
@@ -1,3 +1,38 @@
 class HashTable {
-    
+    constructor(size){
+        this.table = new Array(size)
+        this.size = size
+    }
+
+    hash(key){
+        let total = 0
+        for(let i = 0; i < key.length; i++) {
+            total += key.charCodeAt(i)
+        }
+        return total % this.size
+    }
+
+    set(key, value){
+        const index = this.hash(key)
+        this.table[index] = value
+    }
+
+    get(key){
+        const index = this.hash(key)
+        return this.table[index]
+    }
+
+    display(){
+        for (let i = 0; i < this.table.length; i++) {
+            if (this.table[i]) {
+                console.log(i, this.table[i])
+            }
+        }
+    }
+
 }
+
+const hashTable = new HashTable(50)
+hashTable.set("name", "jospin")
+hashTable.set("age", 24)
+hashTable.display()
