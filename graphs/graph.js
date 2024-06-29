638ae842f44205f90ba40c8632fc79c5c5dec57e
jospin6 <jospinndagano1@gmail.com>
2024-06-29 17:55:09 +0200

    Add the addEdge method to the graph

--- graphs/graph.js
+++ graphs/graph.js
@@ -2,9 +2,19 @@ class Graph {
     constructor(){
         this.adjacencyList = {}
     }
-    addVertix(vertix){
-        if (!this.adjacencyList[vertix]) {
-            this.adjacencyList[vertix] = new set()
+    addVertix(vertex){
+        if (!this.adjacencyList[vertex]) {
+            this.adjacencyList[vertex] = new set()
         } 
+    }
+    addEdge(vertex1, vertex2){
+        if (!this.adjacencyList[vertex1]) {
+            this.addVertix(vertex1)
+        }
+        if (!this.adjacencyList[vertex2]) {
+            this.addVertix(vertex2)
+        }
+        this.adjacencyList[vertex1].add(vertex2)
+        this.adjacencyList[vertex2].add(vertex1)
     }
 }
