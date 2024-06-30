e75bf6526dac22395de68b0437dd9466c3eb12b0
jospin6 <jospinndagano1@gmail.com>
2024-06-30 13:29:30 +0200

    Add breadth first search to the tree

--- trees/binarySearchTree.js
+++ trees/binarySearchTree.js
@@ -53,6 +53,20 @@ class BinarySearchTree {
             this.depthFirstSearch(root.right)
         }
     }
+    breadthFirstSearch(){
+        let queue = []
+        queue.push(this.root)
+        while (queue.length) {
+            let current = queue.shift()
+            console.log(current.value)
+            if (current.left) {
+                queue.push(current.left)
+            }
+            if (current.right) {
+                queue.push(current.right)
+            }
+        }
+    }
 }
 
 const bst = new BinarySearchTree()
