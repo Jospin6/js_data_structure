b19c91961faa0a0e95957c40665a8bd4cd0c9e29
jospin6 <jospinndagano1@gmail.com>
2024-07-03 09:35:12 +0200

    Add hasEdge method to the graph class

--- graphs/graph.js
+++ graphs/graph.js
@@ -17,6 +17,13 @@ class Graph {
         this.adjacencyList[vertex1].add(vertex2)
         this.adjacencyList[vertex2].add(vertex1)
     }
+    hasEdge(vertex1, vertex2) {
+        return (
+            this.adjacencyList[vertex1].has(vertex2) &&
+            this.adjacencyList[vertex2].has(vertex1)
+        )
+    }
+
     display(){
         for (const vertex in this.adjacencyList) {
             console.log(vertex + "->" + this.adjacencyList[vertex])
